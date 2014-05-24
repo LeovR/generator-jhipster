c2e00374176eb0b128666d55fdfba831466db8ef
Jan Marten <jan.marten@gmx.de>
2014-05-24 16:39:23 +0200

    Save testDirectory in variable

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,11 +3,12 @@
 
 var path    = require('path');
 var helpers = require('yeoman-generator').test;
+var testDirectory = path.join(__dirname, 'temp');
 
 
 describe('jhipster generator', function () {
   beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+    helpers.testDirectory(testDirectory, function (err) {
       if (err) {
         return done(err);
       }
